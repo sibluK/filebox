41748fc382b5af982e82da2920ac397a2cc82a99
sibluK <nedask465@gmail.com>
2025-03-24 22:59:42 +0200

    package start script

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
+
+interface FileUploadProps {
+    
+}
+
+export default function FileUpload() {
+
+    const [file, setFile] = useState<File | null>(null);
+    const [uploadedFileUrl, setUploadedFileUrl] = useState<string>("");
+
+    async function handleFileUpload(e: React.FormEvent) {
+        e.preventDefault();
+        if (file) {
+            try {
+                const response = await axios.get("/generate-url");
+                const url = response.data.url;
+                await axios.put(url, file, {
+                    headers: {
+                        "Content-Type": file.type
+                    }
+                });
+
+                const photoUrl = url.split('?')[0];
+                setUploadedFileUrl(photoUrl);
+                console.log(photoUrl);
+            } catch (error) {
+                console.error("Failed to generate upload URL", error);
+            }
+        } else {
+            console.log("No file selected");
+        }
+    }
+
+    function handleFileInput(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.files && e.target.files.length > 0) {
+            setFile(e.target.files[0]);
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={handleFileUpload}>
+                <input onChange={handleFileInput} type="file" accept="image/*"/>
+                <button type="submit">Upload</button>
+            </form>
+
+        </>
+    )
+
+}
--- src/pages/Files.tsx
+++ src/pages/Files.tsx
@@ -3,38 +3,7 @@ import axios from 'axios'
 
 export default function Files() {
 
-    const [file, setFile] = useState<File | null>(null);
     const [uploadedFileUrl, setUploadedFileUrl] = useState<string>("");
-
-    async function handleFileUpload(e: React.FormEvent) {
-        e.preventDefault();
-        if (file) {
-            try {
-                const response = await axios.get("/generate-url");
-                const url = response.data.url;
-                await axios.put(url, file, {
-                    headers: {
-                        "Content-Type": file.type
-                    }
-                });
-
-                // Extract the photo URL from the signed URL
-                const photoUrl = url.split('?')[0];
-                setUploadedFileUrl(photoUrl);
-                console.log(photoUrl);
-            } catch (error) {
-                console.error("Failed to generate upload URL", error);
-            }
-        } else {
-            console.log("No file selected");
-        }
-    }
-
-    function handleFileInput(e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.target.files && e.target.files.length > 0) {
-            setFile(e.target.files[0]);
-        }
-    }
 
     return (
         <>
