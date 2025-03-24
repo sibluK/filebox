9f0996e524d0f5560739cb40cada8cb4c5a58236
sibluK <nedask465@gmail.com>
2025-03-25 00:05:08 +0200

    file upload component: error state

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface FileUploadProps {
     setFileURL: (url: string) => void;
@@ -8,42 +8,65 @@ interface FileUploadProps {
 export default function FileUpload({ setFileURL } : FileUploadProps) {
 
     const [file, setFile] = useState<File | null>(null);
+    const [error, setError] = useState<string>("");
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
     async function handleFileUpload(e: React.FormEvent) {
         e.preventDefault();
         if (file) {
             try {
+                // Get the url for uploading to S3 from the server 
                 const response = await axios.get("https://filebox-pe5y.onrender.com/generate-url");
                 const url = response.data.url;
+
+                // Send the uploaded file to S3 bucket
                 await axios.put(url, file, {
                     headers: {
                         "Content-Type": file.type
                     }
                 });
 
+                // Get and set the file url for displaying the uploaded image
                 const photoUrl = url.split('?')[0];
                 setFileURL(photoUrl);
-                console.log(photoUrl);
+                setError("");
+
+                // Clear the file input
+                if (fileInputRef.current) {
+                    fileInputRef.current.value = "";
+                }
+                setFile(null);
+
             } catch (error) {
                 console.error("Failed to generate upload URL", error);
+                setError("Failed to generate upload URL")
             }
         } else {
-            console.log("No file selected");
+            setError("No file selected")
         }
     }
 
     function handleFileInput(e: React.ChangeEvent<HTMLInputElement>) {
         if (e.target.files && e.target.files.length > 0) {
             setFile(e.target.files[0]);
+            setError("");
         }
     }
 
     return (
         <>
-            <form onSubmit={handleFileUpload}>
-                <input onChange={handleFileInput} type="file" accept="image/*"/>
-                <button type="submit">Upload</button>
-            </form>
+            <div className="file-upload-wrapper">
+                <form onSubmit={handleFileUpload}>
+                    <input ref={fileInputRef} onChange={handleFileInput} type="file" accept="image/*"/>
+                    <button type="submit">Upload</button>
+                </form>
+                {error && (
+                    <div className="error-wrapper">
+                        <span>{error}</span>
+                    </div>
+                )}
+
+            </div>
 
         </>
     )
