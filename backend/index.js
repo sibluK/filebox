08348c91c0bb34954adc360f3760717af77f8b6c
sibluK <nedask465@gmail.com>
2025-03-25 19:08:29 +0200

    neon db postgresql connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import generateUploadURL from './s3.js';
 import cors from 'cors'
+import pkg from 'pg';
 
+
+// App config
 const app = express();
+
+app.use(express.json());
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -10,6 +15,10 @@ app.use(cors({
     allowedHeaders: 'Content-Type'
 }));
 
+const { Pool } = pkg;
+const pool = new Pool({ connectionString: process.env.NEON_POSTGRESQL_DB_STRING });
+
+// Endpoints
 app.get('/generate-url', async (req, res) => {
     try {
         const url = await generateUploadURL();
@@ -18,6 +27,19 @@ app.get('/generate-url', async (req, res) => {
         res.status(500).send({ error: 'Failed to generate upload URL' });
     }
 });
+
+app.get('/users/:id/files', async (req, res) => {
+
+    var user_id = req.params.id;
+
+    try {
+        const { rows } = await pool.query(`SELECT * FROM user_files WHERE user_id = ${user_id}`)
+        res.json(rows);
+    } catch (error) {
+        console.log("Failed to fetch user files");
+        res.status(500).json({ error: 'Interal Server Error'})
+    }
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
