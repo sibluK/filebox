00520bd027d5a63f243bcd44b024a897058f7c77
sibluK <nedask465@gmail.com>
2025-04-02 14:42:56 +0300

    authorization header allow cors

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,8 @@ app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type'
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
 
 app.use(verifyJwt);
